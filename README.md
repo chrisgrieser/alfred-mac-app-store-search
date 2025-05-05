02ecdac54d9a8a69b5044c00e0682115bfb425c6
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-05-05 16:48:46 +0200

    init: bootstrap

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
-# {{workflow-name}}
-![GitHub downloads](https://img.shields.io/github/downloads/chrisgrieser/{{workflow-id}}/total?label=GitHub%20downloads&style=plastic&logo=github)
-![Alfred Gallery downloads](https://img.shields.io/badge/dynamic/yaml?url=https%3A%2F%2Fraw.githubusercontent.com%2Fchrisgrieser%2F.config%2Frefs%2Fheads%2Fmain%2FAlfred.alfredpreferences%2Falfred-workflow-download-count.yaml&style=plastic&logo=alfred&label=Gallery%20downloads&color=%235C1F87&query={{workflow-id}})
-![Latest release](https://img.shields.io/github/v/release/chrisgrieser/{{workflow-id}}?label=Latest%20release&style=plastic)
+# Mac App Store search for Alfred
+![GitHub downloads](https://img.shields.io/github/downloads/chrisgrieser/alfred-mac-app-store-search/total?label=GitHub%20downloads&style=plastic&logo=github)
+<!-- ![Alfred Gallery downloads](https://img.shields.io/badge/dynamic/yaml?url=https%3A%2F%2Fraw.githubusercontent.com%2Fchrisgrieser%2F.config%2Frefs%2Fheads%2Fmain%2FAlfred.alfredpreferences%2Falfred-workflow-download-count.yaml&style=plastic&logo=alfred&label=Gallery%20downloads&color=%235C1F87&query=alfred-mac-app-store-search)-->
+![Latest release](https://img.shields.io/github/v/release/chrisgrieser/alfred-mac-app-store-search?label=Latest%20release&style=plastic)
 
-{{workflow-description}}
+Search apps available in the Mac App Store.
 
-- [ ] Showcase image to be inserted here.
+<img alt="Showcase" width=70% src="https://github.com/user-attachments/assets/9c2ebf78-4983-4a90-a212-acaf0e3e7999">
 
 ## Usage
-- ((Do this)) via the keyword `xxx`.
-- Press the [hotkey](https://www.alfredapp.com/help/workflows/triggers/hotkey/)
-  to ((do this))
+Search the Mac App Store for apps via the keyword `mas`.
+- <kbd>⏎</kbd> Open the result in `App Store.app`.
+- <kbd>⌘⏎</kbd> Open the developer's website, if available.
+- <kbd>⌥⏎</kbd> Copy the App Store URL to your clipboard.
+- <kbd>⌘Y</kbd> Quick Look a screenshot of the app, if available.
+
+## Homebrew packages
+If you use homebrew, you might also be interested in [this workflow to search
+homebrew packages](https://alfred.app/workflows/chrisgrieser/homebrew-search/).
+
+> [!TIP]
+> You can set both of them to the same keyword to simultaneously search homebrew
+> and the Mac App Store.
 
 ## Installation
-[➡️ Download the latest release.](https://github.com/{{repo}}/releases/latest)
+[➡️ Download the latest release.](https://github.com/chrisgrieser/alfred-mac-app-store-search/releases/latest)
 
 ## About the developer
 In my day job, I am a sociologist studying the social mechanisms underlying the
---
+++ scripts/mas-search.js
@@ -0,0 +1,155 @@
+#!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const standardApp = Application.currentApplication();
+standardApp.includeStandardAdditions = true;
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @param {string} url @return {string} */
+function httpRequest(url) {
+	const queryURL = $.NSURL.URLWithString(url);
+	const data = $.NSData.dataWithContentsOfURL(queryURL);
+	return $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding).js;
+}
+
+/**
+ * @typedef {Object} MacAppStoreResult
+ * @property {string} trackName - Name of the app.
+ * @property {string} bundleId
+ * @property {string} version
+ * @property {string} trackViewUrl - URL to the app's page in the Mac App Store.
+ * @property {string} description
+ * @property {string} sellerName - Developer or company name.
+ * @property {string} sellerUrl - website of developer
+ * @property {string} currentVersionReleaseDate - date string
+ * @property {number} averageUserRating - Average user rating (0–5 scale).
+ * @property {number} userRatingCount - Total number of user ratings.
+ * @property {string[]} screenshotUrls - List of screenshot image URLs.
+ * @property {string} artworkUrl100 - URL to 100x100 app icon image.
+ * @property {string} artworkUrl60
+ * @property {string} formattedPrice - App price as a formatted string (e.g. "$4.99").
+ * @property {string} currency - Currency code (e.g. "USD").
+ * @property {number} price
+ */
+
+/**
+ * @param {Date} absDate
+ * @return {string} relative date
+ */
+function relativeDate(absDate) {
+	const deltaMins = (Date.now() - +absDate) / 1000 / 60;
+	/** @type {"year"|"month"|"week"|"day"|"hour"|"minute"} */
+	let unit;
+	let delta;
+	if (deltaMins < 60) {
+		unit = "minute";
+		delta = Math.floor(deltaMins);
+	} else if (deltaMins < 60 * 24) {
+		unit = "hour";
+		delta = Math.floor(deltaMins / 60);
+	} else if (deltaMins < 60 * 24 * 7) {
+		unit = "day";
+		delta = Math.floor(deltaMins / 60 / 24);
+	} else if (deltaMins < 60 * 24 * 7 * 4) {
+		unit = "week";
+		delta = Math.floor(deltaMins / 60 / 24 / 7);
+	} else if (deltaMins < 60 * 24 * 7 * 4 * 12) {
+		unit = "month";
+		delta = Math.floor(deltaMins / 60 / 24 / 7 / 4);
+	} else {
+		unit = "year";
+		delta = Math.floor(deltaMins / 60 / 24 / 7 / 4 / 12);
+	}
+	const formatter = new Intl.RelativeTimeFormat("en", { style: "short", numeric: "auto" });
+	return formatter.format(-delta, unit);
+}
+
+function ensureCacheFolderExists() {
+	const finder = Application("Finder");
+	const cacheDir = $.getenv("alfred_workflow_cache");
+	if (!finder.exists(Path(cacheDir))) {
+		console.log("Cache directory does not exist and is created.");
+		const cacheDirBasename = $.getenv("alfred_workflow_bundleid");
+		const cacheDirParent = cacheDir.slice(0, -cacheDirBasename.length);
+		finder.make({
+			new: "folder",
+			at: Path(cacheDirParent),
+			withProperties: { name: cacheDirBasename },
+		});
+	}
+}
+
+const fileExists = (/** @type {string} */ filePath) => Application("Finder").exists(Path(filePath));
+
+/**
+ * @param {MacAppStoreResult} theApp
+ * @return {string=} cached path
+ */
+function downloadImageOrGetCached(theApp) {
+	const arkwork = theApp.artworkUrl60 || theApp.artworkUrl100;
+	if (!arkwork) return "./icon.png"; // use default image of this workflow
+	const imageCache = $.getenv("alfred_workflow_cache");
+	const path = `${imageCache}/${theApp.bundleId}.png`;
+	if (!fileExists(path)) standardApp.doShellScript(`curl --silent '${arkwork}' > '${path}'`);
+	return path;
+}
+
+//──────────────────────────────────────────────────────────────────────────────
+
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const query = argv[0];
+	if (!query) return;
+
+	// CAVEAT this assumes that the device locale is also the app store locale.
+	// This is not always the case, but 99% of the time, so the best we can do.
+	const regionCode = ObjC.unwrap($.NSLocale.currentLocale.objectForKey($.NSLocaleCountryCode));
+	console.log("Region Code:", regionCode); // e.g., "DE", "US"
+
+	// DOCS https://itunes.apple.com/search?term=notion&entity=macSoftware
+	const limit = $.getenv("result_number");
+	const apiURL = `https://itunes.apple.com/search?entity=macSoftware&country=${regionCode}&limit=${limit}&term=${encodeURIComponent(query)}`;
+	const result = JSON.parse(httpRequest(apiURL))?.results;
+	if (!result) return JSON.stringify({ items: [{ title: "Error: No results", valid: false }] });
+	ensureCacheFolderExists();
+
+	const installedApps = standardApp
+		.doShellScript("mdfind 'kMDItemAppStoreHasReceipt == 1 && kMDItemKind == Application'")
+		.split("\r")
+		.map((line) => line.replace(/.*\/(.*)\.app$/, "$1"));
+
+	/** @type {AlfredItem[]} */
+	const apps = result.map((/** @type {MacAppStoreResult} */ app) => {
+		const imagePath = downloadImageOrGetCached(app);
+
+		const subtitle = [
+			app.price > 0 ? app.formattedPrice : "",
+			app.averageUserRating ? `★ ${app.averageUserRating.toFixed(1)}` : null,
+			relativeDate(new Date(app.currentVersionReleaseDate)),
+			app.description,
+		]
+			.filter(Boolean)
+			.join("  ·  ");
+
+		const emoji = installedApps.includes(app.trackName) ? "✅ " : "";
+
+		/** @type {AlfredItem} */
+		const alfredItem = {
+			title: emoji + app.trackName,
+			subtitle: subtitle,
+			arg: app.trackViewUrl,
+			icon: { path: imagePath || "" },
+			quicklookurl: app.screenshotUrls[0] || "",
+			mods: {
+				cmd: {
+					arg: app.sellerUrl || "",
+					valid: Boolean(app.sellerUrl),
+					subtitle: app.sellerUrl ? "⌘: Open " + app.sellerUrl : "⛔ No website found.",
+				},
+			},
+		};
+		return alfredItem;
+	});
+
+	return JSON.stringify({ items: apps });
+}
